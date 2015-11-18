a920c900cbedd552afe5c879d5f49e6345cf8917
apple <apple@99.local>
2015-11-18 09:40:37 +0800

    Initial Commit

---
+++ fangbocun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fangbocun
+//
+//  Created by apple on 15/11/18.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fangbocun/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fangbocun
+//
+//  Created by apple on 15/11/18.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
